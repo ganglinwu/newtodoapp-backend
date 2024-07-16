bda5464accd25596c97f6c771ea593e4d7e1c50e
Pentagon Learning <pentagonlearning@pentagons-mini.lan>
2024-07-16 18:23:58 +0800

    feat(login) - check if user exists
    - returns 401 if does not exist

--- src/routers/loginRouter.ts
+++ src/routers/loginRouter.ts
@@ -11,7 +11,16 @@ loginRouter.get("(.html)?", (req, res) => {
 loginRouter.post("(.html)?", async (req, res) => {
   const { userName, password } = req.body;
   try {
-    const user = await userModel.find({ userName }).exec();
+    const user = await userModel.findOne({ userName }).exec();
+    if (!user) {
+      res
+        .status(401)
+        .json({ message: "Unauthorized Access, no matching user" });
+      logging.info(
+        `Unauthorized Access - REQ BODY:[${req.body}] - URL:[${req.url}] - IP:[${req.socket.remoteAddress}]`,
+      );
+    }
+    }
   } catch (err) {
     res.status(400).json({ message: `${err}` + "username not found" });
     res.sendFile(path.join(__dirname, "..", "..", "/static", "/login.html"));
