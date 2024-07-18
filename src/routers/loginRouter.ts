9e5e76f7fb049d9bc55fe7ee3def13bc2f218489
ganglinwu <ganglin.wu@gmail.com>
2024-07-18 14:15:16 +0800

    feat(login): can perform simple login verification

--- src/routers/loginRouter.ts
+++ src/routers/loginRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { userModel } from "../models/Users";
+import bcrypt from "bcrypt";
 
 export const loginRouter = express.Router();
 
@@ -10,16 +11,24 @@ loginRouter.get("(.html)?", (req, res) => {
 
 loginRouter.post("(.html)?", async (req, res) => {
   const { userName, password } = req.body;
+  const user = await userModel.findOne({ userName }).exec();
+  if (!user) {
+    res.status(401).json({ message: "Unauthorized Access, Login failed" });
+    logging.warn(
+      `Unauthorized Access(No Such User) - REQ BODY:[${req.body}] - URL:[${req.url}] - IP:[${req.socket.remoteAddress}]`,
+    );
+  }
   try {
-    const user = await userModel.findOne({ userName }).exec();
-    if (!user) {
-      res
-        .status(401)
-        .json({ message: "Unauthorized Access, no matching user" });
-      logging.info(
-        `Unauthorized Access - REQ BODY:[${req.body}] - URL:[${req.url}] - IP:[${req.socket.remoteAddress}]`,
+    const result = await bcrypt.compare(password, user!.password);
+    logging.info(user!.password);
+    logging.info(result);
+    if (result) {
+      res.status(200).json({ message: "Login Success" });
+    } else {
+      logging.warning(
+        `Unauthorized Access(PASSWORD DOES NOT MATCH) - REQ BODY:[${req.body}] - URL:[${req.url}] - IP:[${req.socket.remoteAddress}]`,
       );
-    }
+      res.status(401).json({ message: "Unauthorized Access, Login failed" });
     }
   } catch (err) {
     res.status(500).json({ message: "Server error" });
