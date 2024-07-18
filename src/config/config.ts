361aff2cbf3b220359ec2ec01e37f8007aba5992
Pentagon Learning <pentagonlearning@pentagons-mini.lan>
2024-07-18 16:55:03 +0800

    added JWT_SECRET_KEY import from .env

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,3 +8,5 @@ export const TEST = process.env.NODE_ENV === "test";
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
 export const DATABASE_URI = process.env.DATABASE_URI;
+
+export const JWT_SECRETKEY = process.env.JWT_SECRET_KEY;
