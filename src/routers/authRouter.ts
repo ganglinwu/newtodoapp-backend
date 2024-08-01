897abcd85627defe74825920f89196d1a72e5ade
ganglinwu <ganglin.wu@gmail.com>
2024-08-01 16:02:44 +0800

    incomplete: yet to implement fetch/xhr to refresh endpoint

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRETKEY } from "../config/config";
+import { refreshRouter } from "./refreshRouter";
 
 export const authRouter = express.Router();
 
@@ -10,20 +11,20 @@ export const authCheck = function () {
       logging.error("JWT SERET KEY not found!");
       res.status(500).json({ message: "Server error" });
     } else {
-      const jwtToken = req.cookies.jwt;
-      jwt.verify(jwtToken, JWT_SECRETKEY!, function (err, decoded) {
+      const accessToken = req.cookies["accessToken"];
+      jwt.verify(accessToken, JWT_SECRETKEY!, function (err, decoded) {
         if (err === null) {
           res.status(200);
           logging.log(
-            `Incoming - METHOD:[${req.method}] - URL:[${req.url}] - IP:[${req.socket.remoteAddress}] - STATUS:[${res.statusCode}]`,
+            `Outgoing - METHOD:[${req.method}] - URL:[${req.url}] - IP:[${req.socket.remoteAddress}] - STATUS:[${res.statusCode}]`,
           );
         } else {
           if (err.name === "TokenExpiredError") {
-            // refresh token if expired?
-            res.status(403).json({ message: "Token expired." });
             logging.log(
-              `Incoming - METHOD:[${req.method}] - URL:[${req.url}] - IP:[${req.socket.remoteAddress}] - STATUS:[${res.statusCode}] - REASON:[Token expired]`,
+              `Outgoing - METHOD:[${req.method}] - URL:[${req.url}] - IP:[${req.socket.remoteAddress}] - STATUS:[${res.statusCode}] - REASON:[Token expired]`,
             );
+            logging.log(`Checking for refresh token`);
+            return res.redirect("http://localhost:3001/refresh");
           } else if (err.name === "JsonWebTokenError") {
             //  invalid token
             logging.warn(err.message);
