45d701f96ae57bd8d468e0148ec7d1f29b76134d
ganglinwu <ganglin.wu@gmail.com>
2024-08-15 13:16:27 +0800

    refactor: multiple
    - since the data type requires nested arrays and objects, separate
      schemas for nested arrays and objects have been created
    
    - also we have removed timeUpdated field, we will use those
      automatically generated by mongodb

--- src/models/UserProjects.ts
+++ src/models/UserProjects.ts
@@ -2,25 +2,32 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
+// we nest taskSchema inside projectSchema so we declare this before projectSchema
+const taskSchema = new Schema(
+  {
+    taskName: { type: String, required: true },
+    taskDueDate: Date,
+    taskUrgency: String,
+    taskDuration: Number,
+  },
+  { _id: false, timestamps: true },
+);
+
+// we nest projectSchema inside userProjectSchema so we declare this before userProjectSchema
+const projectSchema = new Schema(
+  {
+    projectName: { type: String, required: true },
+    tasks: [taskSchema],
+  },
+  { _id: false, timestamps: true },
+);
+
 const userProjectSchema = new Schema(
   {
-    userName: { type: String, required: true },
-    timeUpdated: { type: Date, default: Date.now },
-    projects: [
-      {
-        projectName: { type: String, required: true },
-        tasks: [
-          {
-            taskName: { type: String, required: true },
-            taskDueDate: Date,
-            taskUrgency: String,
-            taskDuration: Number,
-          },
-        ],
-      },
-    ],
+    userName: { type: String, required: true, unique: true },
+    projects: [projectSchema],
   },
-  { collection: "userprojects" },
+  { collection: "userprojects", timestamps: true },
 );
 export const userProjectModel = mongoose.model(
   "userProject",
