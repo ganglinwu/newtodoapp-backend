e9554e41e0da860f9eed4670e1622d8dc89c5717
ganglinwu <ganglin.wu@gmail.com>
2024-08-19 15:17:28 +0800

    added feedback for successful logout
    - server log
    - json message to client

--- src/routers/logoutRouter.ts
+++ src/routers/logoutRouter.ts
@@ -5,5 +5,8 @@ export const logoutRouter = express.Router();
 logoutRouter.get("(.html)?", (req, res) => {
   res.clearCookie("accessToken");
   res.clearCookie("refreshToken");
-  res.end();
+  res.status(200).json({ message: "Logout Success" });
+  logging.log(
+    `Outgoing - METHOD:[${req.method}] - URL:[${req.url}] - STATUS:[${res.statusCode}]`,
+  );
 });
